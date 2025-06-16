edce6d0c733ad2de3ac2143661d412cbc00bad7a
Alexis2707 <alexis.lambert2707@gmail.com>
2025-06-16 23:31:55 +0200

    Ajout des fonctions pour les dataframes des 3 premieres vis

--- src/preprocess.py
+++ src/preprocess.py
@@ -112,6 +112,7 @@ def get_mean_value(dataframe: pd.DataFrame, column_name: str) -> Union[float, No
     return median_values
 
 
+# Commentaire à supprimer: Permet de faire le graphe de la première vis
 def get_results_by_age_range(dataframe: pd.DataFrame) -> pd.DataFrame:
     '''
     Count the number of patients in different age ranges.
@@ -152,3 +153,74 @@ def get_results_by_age_range(dataframe: pd.DataFrame) -> pd.DataFrame:
             'Total': positive_count + negative_count
         })
     return pd.DataFrame(results_summary)
+
+
+# Commentaire à supprimer: Permet de faire le graphe de la deuxième vis
+def get_heart_rate_median(dataframe: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Calculate the median heart rate for each outcome.
+
+    Args:
+        dataframe (pd.DataFrame): The DataFrame containing the dataset.
+
+    Returns:
+        pd.DataFrame: DataFrame with the median heart rate for each outcome.
+    '''
+    return get_median_value(dataframe, 'Heart rate')
+
+
+def get_blood_sugar_median(dataframe: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Calculate the median blood sugar for each outcome.
+
+    Args:
+        dataframe (pd.DataFrame): The DataFrame containing the dataset.
+
+    Returns:
+        pd.DataFrame: DataFrame with the median blood sugar for each outcome.
+    '''
+    return get_median_value(dataframe, 'Blood sugar')
+
+
+# Commentaire à supprimer: Permet de faire le graphe de la troisième vis
+def filter_systolic_diastolic_blood_pressure(dataframe: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Filter the columns of the dataframe to only include systolic and diastolic blood pressure and result.
+
+    Args:
+        dataframe (pd.DataFrame): The DataFrame containing the dataset.
+
+    Returns:
+        pd.DataFrame: A new DataFrame containing the filtered dataframe.
+    '''
+    filtered_df = dataframe[['Systolic blood pressure', 'Diastolic blood pressure', 'Result']].copy()
+    filtered_df.columns = ['Systolic BP', 'Diastolic BP', 'Result']
+    filtered_df['Result'] = filtered_df['Result'].astype('category')
+    return filtered_df
+
+
+# Commentaire à supprimer: Permet de faire le graphe de la quatrième vis
+def get_ckmb_median(dataframe: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Calculate the median CK-MB for each outcome.
+
+    Args:
+        dataframe (pd.DataFrame): The DataFrame containing the dataset.
+
+    Returns:
+        pd.DataFrame: DataFrame with the median CK-MB for each outcome.
+    '''
+    return get_median_value(dataframe, 'CK-MB')
+
+
+def get_troponin_median(dataframe: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Calculate the median Troponin for each outcome.
+
+    Args:
+        dataframe (pd.DataFrame): The DataFrame containing the dataset.
+
+    Returns:
+        pd.DataFrame: DataFrame with the median Troponin for each outcome.
+    '''
+    return get_median_value(dataframe, 'Troponin')
